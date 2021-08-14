d3b1a7003cb34683ba819b14db69297f8e090f64
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-08-14 18:25:43 -0300

    posicionamento de elementos com bootstrap

--- src/main/java/br/com/gabriel/aluraspringmvc/controller/HomeController.java
+++ src/main/java/br/com/gabriel/aluraspringmvc/controller/HomeController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,6 +16,8 @@ public class HomeController {
     @GetMapping("/home")
     public String home(Model model) {
         Pedido pedido = new Pedido();
+        pedido.setValorNegociado(BigDecimal.valueOf(899.00));
+        pedido.setDataDaEntrega(LocalDate.now());
         pedido.setNomeProduto("Moto E7 Plus 64GB");
         pedido.setUrlImagem("https://m.media-amazon.com/images/I/51pke8P8IjL._AC_SL1000_.jpg");
         pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Moto-Plus-Azul-C%C3%A2mera/dp/B08NFJWM8Y/ref=sr_1_3?dchild=1&keywords=moto+e7+plus&qid=1628972242&sr=8-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147");
