ec8a26fc247b4f62daf2665ddf225be482ecdaae
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-10-17 16:37:43 -0300

    Validação de formulário com Thymeleaf

--- src/main/java/br/com/gabriel/aluraspringmvc/controller/PedidoController.java
+++ src/main/java/br/com/gabriel/aluraspringmvc/controller/PedidoController.java
@@ -5,9 +5,12 @@ import br.com.gabriel.aluraspringmvc.model.dto.RequisicaoPedido;
 import br.com.gabriel.aluraspringmvc.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("pedido")
@@ -22,8 +25,11 @@ public class PedidoController {
     }
 
     @PostMapping("novo")
-    public String novo(RequisicaoPedido reqPedido) {
-        Pedido pedido = reqPedido.toPedido();
+    public String novo(@Valid RequisicaoPedido requisicaoPedido, BindingResult result) {
+        if (result.hasErrors())
+            return "pedido/formulario";
+
+        Pedido pedido = requisicaoPedido.toPedido();
         pedidoRepository.save(pedido);
 
         return "home";
--- src/main/java/br/com/gabriel/aluraspringmvc/model/dto/RequisicaoPedido.java
+++ src/main/java/br/com/gabriel/aluraspringmvc/model/dto/RequisicaoPedido.java
@@ -3,12 +3,20 @@ package br.com.gabriel.aluraspringmvc.model.dto;
 import br.com.gabriel.aluraspringmvc.model.Pedido;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class RequisicaoPedido {
 
+    @NotBlank
     private String nomeProduto;
+
+    @NotBlank
     private String urlProduto;
+
+    @NotBlank
     private String urlImagem;
+
     private String descricao;
 
     public Pedido toPedido() {
